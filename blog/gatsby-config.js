f1d5f812127ac8c6954453fe3a2fec6a3237878c
Michael Rinderle <michael@sofdigital.net>
2022-01-24 18:39:58 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,9 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Michael-Rinderle`,
+    description: `just coding. building new stuff...`,
+    author: `Michael Rinderle`,
+    contact: {
+        name: `Michael Rinderle`,
+        company: `Sof Digital`,
+        address: `Waukesha, WI`
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
