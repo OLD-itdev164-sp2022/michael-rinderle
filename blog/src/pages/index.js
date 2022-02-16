705aab17eef85b612f1f0e7118595dfda551cbea
Michael Rinderle <michael@sofdigital.net>
2022-02-16 00:16:33 -0600

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,37 +1,58 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import { Link } from 'gatsby';
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-import Layout from '../components/layout';
-import Seo from '../components/seo';
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const IndexPage = ({ data }) => (
-    <Layout>
-        <Seo title="Home" />
-        <ul>
-            {
-                data.allContentfulBlogPost.edges.map(edge => (
-                    <li>
-                        <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
-                    </li>
-                ))
-            }
-        </ul>
-    </Layout>
-);
+  <Layout>
+    <Seo title="Home" />
+    <ul>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+              alt="test"
+            />
+          </div>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
+        </li>
+      ))
+    }
+    </ul>
+  </Layout>
+)
 
 export default IndexPage
 
 export const query = graphql`
-    {
-        allContentfulBlogPost {
-            edges {
-                node {
-                    id
-                    title
-                    slug
-                }
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
             }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
+      }
     }
+  }
 `
