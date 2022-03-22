a18e256a48f133666c4de39a41accecfd552c5b8
Michael Rinderle <michael@sofdigital.net>
2022-03-21 22:16:22 -0500

    Completed Activity 7

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
-import styled from 'styled-components'
-import { H1 } from '../Heading'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import { H1 } from '../Heading';
+import { Section } from '../Section';
 
 const Outer = styled.header`
     background: ${props => props.theme.header.backgroundColor};
@@ -26,11 +27,16 @@ const StyledLink = styled(Link)`
 const Header = ({ siteTitle }) => (
     <Outer>  
         <Inner>
-            <H1>
-                <StyledLink to='/'>
-                    { siteTitle }
-                </StyledLink>   
-            </H1>
+            <Section flex>
+                <H1>
+                    <StyledLink to="/">
+                        { siteTitle }
+                    </StyledLink>
+                </H1>
+            </Section>            
+            <Section width={1/12}>
+                Search
+            </Section>
         </Inner>
     </Outer>
 )
